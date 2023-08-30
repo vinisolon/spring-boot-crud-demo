d84854339de07f17fd11b98311b415e0ac9e7c3b
Vinicius Solon <vini.97@live.com>
2023-08-30 17:58:41 -0300

    Project structure

---
+++ src/main/java/br/com/vinisolon/application/SpringBootCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinisolon.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vinisolon/application/SpringBootCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinisolon.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
