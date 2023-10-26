725c9bb5a75be138336d772f4f07d1dd0ccf4700
Vinicius Solon <vini.97@live.com>
2023-10-26 18:37:41 -0300

    Added @Email annotation for requests validation

--- src/main/java/br/com/vinisolon/application/requests/CreateUserRequest.java
+++ src/main/java/br/com/vinisolon/application/requests/CreateUserRequest.java
@@ -1,10 +1,11 @@
 package br.com.vinisolon.application.requests;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreateUserRequest(
         @NotBlank String username,
         @NotBlank String password,
-        @NotBlank String email
+        @NotBlank @Email String email
 ) {
 }
--- src/main/java/br/com/vinisolon/application/requests/UpdateUserRequest.java
+++ src/main/java/br/com/vinisolon/application/requests/UpdateUserRequest.java
@@ -1,5 +1,6 @@
 package br.com.vinisolon.application.requests;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -7,6 +8,6 @@ public record UpdateUserRequest(
         @NotNull Long id,
         @NotBlank String username,
         @NotBlank String password,
-        @NotBlank String email
+        @NotBlank @Email String email
 ) {
 }
