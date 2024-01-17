bd203a89b00b51c4be676a7354973675e69e7817
Vinicius Solon <vini.97@live.com>
2024-01-17 18:38:53 -0300

    Removed @EnableJpaAuditing from main class

--- src/main/java/br/com/vinisolon/application/SpringBootCrudDemoApplication.java
+++ src/main/java/br/com/vinisolon/application/SpringBootCrudDemoApplication.java
@@ -3,9 +3,7 @@ package br.com.vinisolon.application;
 import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootCrudDemoApplication {
 
